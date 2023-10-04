3e69a0797864197a93c1621204935b5c4539da35
Luis Keny Lucero Balvin <lucerobalvinluiskeny@gmail.com>
2023-10-03 19:06:13 -0500

    (Correccion): logica de los servicios de accesos dentro del componente home ts

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.accessService.getAll().subscribe(data => {
-      this.moduleView = data;
+      this.moduleView = data.filter(v => v.id_acceso_padre == null);
     });
   }
 
